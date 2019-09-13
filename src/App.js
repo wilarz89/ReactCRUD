73a2d4c3269f551adb2c6d3f80b73389be625671
Wilson Ariza <wilson.ariza@bewe.io>
2019-09-12 20:29:01 -0500

    added add, create,delete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UserTable from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm'
 
 const App = () => {
+  const usersData = [
+    { id: 1, name: 'Wilson', username: 'wilarz89' },
+    { id: 2, name: 'Javier', username: 'jh2kh' },
+    { id: 3, name: 'Lorena', username: 'vlorena' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
+  const addUser = user => {
+    user.id = users.length + 1
+    setUsers([...users, user])
+  }
+
+  const deleteUser = id => {
+    setUsers(users.filter(user => user.id !== id))
+  }
+
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <AddUserForm addUser={addUser} />
         </div>
         <div className="flex-large">
           <h2>View users</h2>
+          <UserTable users={users} deleteUser={deleteUser} />
         </div>
       </div>
     </div>
   )
 }
 
-export default App;
+export default App
